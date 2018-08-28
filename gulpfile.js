d9838088e2301b57695ebe0b505836c3293db383
JJ Ying <yingjunjiu@gmail.com>
2018-08-28 12:07:07 +0800

    Added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp = require('gulp'), gulpLoadPlugins = require('gulp-load-plugins'), plugins = gulpLoadPlugins();
+var gulp = require('gulp'), gulpLoadPlugins = require('gulp-load-plugins'), plugins = gulpLoadPlugins()
 
 var cdnUrl = [
 	// [ 'assets/', 'https://s.anw.red/' ],
 	[	'assets/vue.js', 'assets/vue.min.js' ]
-];
+]
 
 gulp.task('default', function() {
 
@@ -14,22 +14,27 @@ gulp.task('default', function() {
     	}))
 		.pipe(plugins.batchReplace(cdnUrl))
     .pipe(plugins.htmlmin({collapseWhitespace: true}))
-		.pipe(gulp.dest('builds'));
+		.pipe(gulp.dest('builds'))
 
 	gulp.src('assets/main.js')
     .pipe(plugins.minify({
 			ext:{
-            min:'.js'
-        },
-				noSource: true
+      	min:'.js'
+      },
+			noSource: true
 		}))
     .pipe(gulp.dest('builds'))
 
+		gulp.src('Better-Shadow.sketchplugin/')
+	  	.pipe(plugins.zip('Better-Shadow-Latest.zip'))
+	    .pipe(gulp.dest('Releases'))
+
 	gulp.src('assets/*.css')
   	.pipe(plugins.cleanCss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('builds'));
-});
+    .pipe(gulp.dest('builds'))
+
+})
 
 gulp.task('watch', function() {
-	gulp.watch(['*','*/*'], ['default']);
- });
+	gulp.watch(['*','*/*'], ['default'])
+ })
